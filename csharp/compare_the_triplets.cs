b18f2e8ebadb912231138f0e4c531ef33111b61d
Gustavo <39868190+gustavo-f-b@users.noreply.github.com>
2023-11-28 13:06:47 -0300

    feat(compare_the_triplets)
    Add resolve the solved problem Compare The Triplets

---
+++ csharp/compare_the_triplets.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<int> CompareTriplets(List<int> a, List<int> b)
+    {
+        var listResult = new List<int>();
+        
+        var pointA = 0;
+        var pointB = 0;
+        
+        for (int i = 0; i < a.Count; i++)
+        {
+            if(a[i] !=  b[i])
+            {
+                pointA += a[i] > b[i] ? 1 : 0;
+                pointB += a[i] < b[i] ? 1 : 0;
+            }
+        }
+        
+        listResult.Add(pointA);
+        listResult.Add(pointB);
+        
+        return listResult;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = Result.CompareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
